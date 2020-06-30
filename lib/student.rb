5719354de326ab44c6f25d9761bd456069d7a8f5
Michael Harris <mwharris7@gmail.com>
2020-06-30 08:14:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,14 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  database_connection = SQLite3::Database.new('db/students.db')
+  attr_accessor :name, :grade
+  attr_reader :id
 
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  
 end
