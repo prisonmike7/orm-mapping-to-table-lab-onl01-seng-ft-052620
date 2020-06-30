356a143683dc9bfce70e3ca974d5294b8483b2fc
Michael Harris <mwharris7@gmail.com>
2020-06-30 08:09:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
   database_connection = SQLite3::Database.new('db/students.db')
-  
+
 end
