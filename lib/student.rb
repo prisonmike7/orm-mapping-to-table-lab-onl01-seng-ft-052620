a67bfb69f99c6b58e61aa078e6d455da90e600cb
Michael Harris <mwharris7@gmail.com>
2020-06-30 08:24:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
-      )
+      );
     SQL
 
     DB[:conn].execute(sql)
@@ -27,8 +27,11 @@ class Student
 
   def self.drop_table
     sql = <<-SQL
-
+      DROP TABLE students;
     SQL
+
+    DB[:conn].execute(sql)
+
   end
 
   def save
