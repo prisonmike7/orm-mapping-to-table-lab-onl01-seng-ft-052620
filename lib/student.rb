9ccd90797abf68c0fa8c101cf31a051bfcfba9d1
Michael Harris <mwharris7@gmail.com>
2020-06-30 08:20:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,12 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-
+      CREATE TABLE students
+      (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
     SQL
   end
 
